fc8eb4616bca0e9cb1035ad08035bbe7d58f2d47
Konovalov Vadim <Konovalov-Vadim2008@yandex.ru>
2024-07-08 17:16:16 +0300

    Создание моделей category и products

---
+++ app/models/category.py
@@ -0,0 +1,15 @@
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+
+
+class Category(Base):
+    __tablename__: str = 'categories'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    name = Column(String)
+    slug = Column(String, unique=True, index=True)
+    is_active = Column(Boolean, default=True)
+
+
